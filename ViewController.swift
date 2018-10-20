1891fb4f63620d4549a28a333a9f7fc894689035
Christian Jimenez <Chris_Stain@Christians-Mini-24.frontierlocal.net>
2018-10-19 22:01:28 -0700

    ViewController

---
+++ ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Magic8ball
+//
+//  Created by Christian Jimenez on 9/11/18.
+//  Copyright © 2018 Christian Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+   
+    
+    var randomBallNumber: Int = 0
+    
+    let array = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        randomBallNumber = Int.random(in: 0 ... 4)
+        imageView.image = UIImage(named: array[randomBallNumber])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0 ... 4)
+        imageView.image = UIImage(named: array[randomBallNumber])
+        print(randomBallNumber)
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+
+}
+
